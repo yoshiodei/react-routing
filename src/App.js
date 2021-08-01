2b1acc6003af8b6a2a3d2257e9b602d1077bf977
yoshiodei <yoshiodei@yahoo.com>
2021-08-01 22:09:27 +0000

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import {BrowserRouter as Router, Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './Pages/Home';
+import Register from './Pages/Register';
 import StartUp from './Pages/StartUp';
+import Login from './Pages/Login';
+
+
+
 
 
 function App() {
@@ -9,7 +14,9 @@ function App() {
       <Router >
         <Route path="/" exact component={Home}/>
         <Route path="/startup" exact component={StartUp}/>
-
+        <Route path="/register" exact component={Register}/>
+        <Route path="/login" exact component={Login}/>
+        
       </Router>  
     </>
   );
